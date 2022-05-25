1e7d0189103a12fc7efa4bbdc81af6a496e63002
mayuri6355 <mayuriptl94@gmail.com>
2022-05-25 10:46:56 +0530

    add new fiels

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import './App.css';
 import { increseNum, decreseNum} from './actions';
 import {useDispatch,useSelector } from 'react-redux'
 
+import React from 'react';
+import { Counter } from '../src/features/counter/Counter'
+
+
 function App() {
   const dispatch=useDispatch();
   const val=useSelector((state)=>state.upDown)
   return (
     <div className="App">
-    <h1>{val}</h1>
+    {/* <h1>{val}</h1>
     <input type='button' value='Increment' onClick={()=>{dispatch(increseNum())}}></input>
-    <input type='button' value='Decrement' onClick={()=>{dispatch( decreseNum())}}></input>
+    <input type='button' value='Decrement' onClick={()=>{dispatch( decreseNum())}}></input> */}
+
+    <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Counter />
+      </header>
     </div>
   );
 }
--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
 import rootReducer from "./reducers";
 import { createStore } from "redux";
-const store=createStore(rootReducer);
-export default store;
+
+ const store=createStore(rootReducer);
+ export default store;
